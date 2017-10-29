2acfc534245e1d0f2cc8187ea3d78771709fe763
James Logue <logueja@gmail.com>
2017-10-29 14:37:14 -0600

    Rename and refactor constructor, storage, card create & delete functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,66 +1,66 @@
 $(document).ready(populateExistingCards(findExistingCards()));
-$('.save-btn').on('click', createCard);
-$('.crd-ctnr').on('click', '.del-btn', deleteIdeaCard);
-$('.crd-ctnr').on('click', '.up-btn', upvoteQuality);
-$('.crd-ctnr').on('click', '.down-btn', downvoteQuality);
-$('.crd-ctnr').on('blur', '.crd-title', changeIdeaTitle);
-$('.crd-ctnr').on('blur', '.crd-task', changeIdeaBody);
-$('.crd-ctnr').on('keypress', '.crd-title', updateTitle);
-$('.crd-ctnr').on('keypress', '.crd-task', updateBody);
+var $crdCtnr = $('.crd-ctnr');
+
+$('.save-btn').on('click', newCrd);
+$crdCtnr.on('click', '.del-btn', delCrd);
+$crdCtnr.on('click', '.up-btn', voteUp);
+$crdCtnr.on('click', '.down-btn', voteDown);
+$crdCtnr.on('blur', '.crd-title', changeIdeaTitle);
+$crdCtnr.on('blur', '.crd-task', changeIdeaBody);
+$crdCtnr.on('keypress', '.crd-title', updateTitle);
+$crdCtnr.on('keypress', '.crd-task', updateBody);
 $('srch-input').on('keyup', searchString);
 
-function IdeaCardObject(id, title, body) {
+function ToDoCrd(id, title, body, quality) {
   this.id = id;
   this.title = title;
   this.body = body;
   this.quality = 'swill';
 }
 
-function createCard() {
+function newCrd() {
   event.preventDefault();
-  var newCard = new IdeaCardObject(id = Date.now(), $('.title-input').val(), $('.task-input').val());
-  sendCardToLocalStorage(newCard);
+  var newCrd = new ToDoCrd(Date.now(), $('.title-input').val(), $('.task-input').val());
+  toSto(newCrd);
+  prependCrd(newCrd);
+  clrInp();
 }
 
-function sendCardToLocalStorage(newCard) {
-  var stringifiedObject = JSON.stringify(newCard);
-  localStorage.setItem(newCard.id, stringifiedObject);
-  retrieveObjPutOnPage(newCard.id);
+function toSto(crd) {
+  var stringifiedObject = JSON.stringify(crd);
+  localStorage.setItem(crd.id, stringifiedObject);
 }
 
-function sendUpdatesToLocalStorage(updatedObject) {
-  var stringifiedObject = JSON.stringify(updatedObject);
-  localStorage.setItem(updatedObject.id, stringifiedObject);
+function fromSto(id) {
+  var thsObj = localStorage.getItem(id);
+  var $obj = JSON.parse(thsObj);
+  return $obj;
 }
 
-function retrieveObjPutOnPage(id) {
-  var retrievedObject = localStorage.getItem(id);
-  var parsedObject = JSON.parse(retrievedObject);
-  prependIdeaCard(parsedObject.id, parsedObject.title, parsedObject.body, parsedObject.quality);
-  clearInputs();
+function getId(crd) {
+  var crdId = parseInt($(crd).closest('article').attr('id'));
+  return crdId
 }
 
+function delCrd() {
+  $(this).closest('article').remove();
+  localStorage.removeItem(getId(this));
+}
+
+function clrInp() {
+  $('.title-input').val('');
+  $('.task-input').val('');
+}
+
+
+
+
+//don't need dual loops of populateExisting && findExisting
 function populateExistingCards(keyValues) {
   for (var i = 0; i < keyValues.length; i++) {
-    retrieveObjPutOnPage(keyValues[i].id);
+    var thisCrd = fromSto(keyValues[i].id);
+    prependCrd(thisCrd)
   }
-}
-
-function prependIdeaCard(id, title, body, quality) {
-  $('.crd-ctnr').prepend(
-    `<article class="2do-crd" id="${id}">
-      <header class="crd-hdr">
-        <h2 class="crd-title" contenteditable="true">${title}</h2> 
-        <button class="del-btn" name="delete button"></button>
-      </header>
-      <p class="crd-task" contenteditable="true">${body}</p>
-      <footer class="crd-ftr">
-        <button class="up-btn" name="more important"></button>
-        <button class="down-btn" name="less important"></button>
-        <h3 class="imp-title">Importance: <span class="imp-val">${quality}</span></h3>
-      </footer>
-    </article>`
-  );
 }
 
 function findExistingCards() {
@@ -72,24 +72,11 @@ function findExistingCards() {
   return keyValues;
 }
 
-function addIdeaCard() {
-  event.preventDefault();
-  prependIdeaCard();
-  clearInputs();
-}
 
-function deleteIdeaCard() {
-  $(this).closest('article').remove();
-  var cardId = $(this).closest('article').attr('id');
-  localStorage.removeItem(JSON.parse(cardId));
-}
 
-function clearInputs() {
-  $('.title-input').val('');
-  $('.task-input').val('');
-}
+// these two (voteUp & voteDown) should be a single function w/ multiple args
 
-function upvoteQuality() {
+function voteUp() {
   var qualityArray = ['swill', 'plausible', 'genius'];
   var currentQuality = $(this).siblings('.imp-val').text();
   var currentIndex = qualityArray.indexOf(currentQuality);
@@ -101,12 +88,12 @@ function upvoteQuality() {
   }
 
   var cardId = parseInt($(this).closest('article').attr('id'));
-  var cardObject = getObjectFromStorage(cardId);
+  var cardObject = fromSto(cardId);
   cardObject.quality = qualityArray[currentIndex];
-  sendUpdatesToLocalStorage(cardObject);
+  toSto(cardObject);
 }
 
-function downvoteQuality() {
+function voteDown() {
   var qualityArray = ['swill', 'plausible', 'genius'];
   var currentQuality = $(this).siblings('.imp-val').text();
   var currentIndex = qualityArray.indexOf(currentQuality);
@@ -119,36 +106,15 @@ function downvoteQuality() {
   }
   
   var cardId = parseInt($(this).closest('article').attr('id'));
-  var cardObject = getObjectFromStorage(cardId);
+  var cardObject = fromSto(cardId);
   cardObject.quality = qualityArray[currentIndex];
-  sendUpdatesToLocalStorage(cardObject);
+  toSto(cardObject);
 }
 
-function getObjectFromStorage(cardId) {
-  var retrievedObject = localStorage.getItem(cardId);
-  var parsedObject = JSON.parse(retrievedObject);
-  return parsedObject;
-}
 
-function changeIdeaTitle() {
-  var currentTitle = $(this).text();
-  var cardId = parseInt($(this).closest('article').attr('id'));
-  var cardObject = getObjectFromStorage(cardId);
-  cardObject.title = currentTitle;
-  sendUpdatesToLocalStorage(cardObject);
-}
 
-function changeIdeaBody() {
-  var currentBody = $(this).text();
-  var cardId = parseInt($(this).closest('article').attr('id'));
-  var cardObject = getObjectFromStorage(cardId);
-  cardObject.body = currentBody;
-  sendUpdatesToLocalStorage(cardObject);
-}
 
-function clearAllCards() {
-  $('.crd-ctnr').text('');
-}
+// this can be shorterrr
 
 function searchString() {
   var cardObjectsArray = findExistingCards();
@@ -163,14 +129,39 @@ function searchString() {
   populateExistingCards(filteredCards);
 }
 
+function clearAllCards() {
+  $('.crd-ctnr').text('');
+}
+
+
+
+/// refactor these two functions into one which takes title or body arg
+function changeIdeaTitle() {
+  var currentTitle = $(this).text();
+  var cardId = parseInt($(this).closest('article').attr('id'));
+  var cardObject = fromSto(cardId);
+  cardObject.title = currentTitle;
+  toSto(cardObject);
+}
+
+function changeIdeaBody() {
+  var currentBody = $(this).text();
+  var cardId = parseInt($(this).closest('article').attr('id'));
+  var cardObject = fromSto(cardId);
+  cardObject.body = currentBody;
+  toSto(cardObject);
+}
+
+
+/// also these... maybe these and prev 2 can be one function
 function updateTitle(event) {
   if (13 == event.keyCode) {
     event.preventDefault();
     $('.crd-title').blur();
     var cardId = parseInt($(this).closest('article').attr('id'));
-    var cardObject = getObjectFromStorage(cardId);
+    var cardObject = fromSto(cardId);
     cardObject.title = $(this).closest('.crd-title').text();
-    sendUpdatesToLocalStorage(cardObject);
+    toSto(cardObject);
   }
 }
 
@@ -179,8 +170,28 @@ function updateBody(event) {
     event.preventDefault();
     $('.crd-task').blur();
     var cardId = parseInt($(this).closest('article').attr('id'));
-    var cardObject = getObjectFromStorage(cardId);
+    var cardObject = fromSto(cardId);
     cardObject.body = $(this).closest('.crd-task').text();
-    sendUpdatesToLocalStorage(cardObject);
+    toSto(cardObject);
   }
+}
+
+
+
+
+function  prependCrd(crd) {
+  $('.crd-ctnr').prepend(
+    `<article class="2do-crd" id="${crd.id}">
+      <header class="crd-hdr">
+        <h2 class="crd-title" contenteditable="true">${crd.title}</h2> 
+        <button class="del-btn" name="delete button"></button>
+      </header>
+      <p class="crd-task" contenteditable="true">${crd.body}</p>
+      <footer class="crd-ftr">
+        <button class="up-btn" name="more important"></button>
+        <button class="down-btn" name="less important"></button>
+        <h3 class="imp-title">Importance: <span class="imp-val">${crd.quality}</span></h3>
+      </footer>
+    </article>`
+  );
 }
