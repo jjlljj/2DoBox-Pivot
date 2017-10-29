da2fca1c9f050bcd8b7a2405bef59f9bf18f0994
James Logue <logueja@gmail.com>
2017-10-29 16:55:39 -0600

    Restore functionality, refactor and rename idea quality function, future commit will adapt to rate importance feature

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,15 @@ var $crdCtnr = $('.crd-ctnr');
 
 $('.save-btn').on('click', newCrd);
 $crdCtnr.on('click', '.del-btn', delCrd);
-$crdCtnr.on('click', '.up-btn', voteUp);
-$crdCtnr.on('click', '.down-btn', voteDown);
+
+$crdCtnr.on('click', '.up-btn', function(){
+  changeImp(this, 'more')
+});
+
+$crdCtnr.on('click', '.down-btn', function() {
+  changeImp(this, 'less')
+});
+
 $crdCtnr.on('blur', '.crd-title', changeIdeaTitle);
 $crdCtnr.on('blur', '.crd-task', changeIdeaBody);
 $crdCtnr.on('keypress', '.crd-title', updateTitle);
@@ -27,18 +34,20 @@ function newCrd() {
 }
 
 function toSto(crd) {
-  var stringifiedObject = JSON.stringify(crd);
-  localStorage.setItem(crd.id, stringifiedObject);
+      // IS IT BETTER TO DO THIS:
+//  var stringObj = JSON.stringify(crd);
+//  localStorage.setItem(crd.id, stringObj);
+     // OR THIS:
+  localStorage.setItem(crd.id, JSON.stringify(crd));
 }
 
 function fromSto(id) {
-  var thsObj = localStorage.getItem(id);
-  var $obj = JSON.parse(thsObj);
+  var $obj = JSON.parse(localStorage.getItem(id));
   return $obj;
 }
 
 function getId(crd) {
-  var crdId = parseInt($(crd).closest('article').attr('id'));
+  var crdId = $(crd).closest('article').attr('id');
   return crdId
 }
 
@@ -48,10 +57,8 @@ function delCrd() {
 }
 
 function clrInp() {
-  $('.title-input').val('');
-  $('.task-input').val('');
+  $('.title-input, .task-input').val('');
 }
-
 
 
 
@@ -72,43 +79,21 @@ function findExistingCards() {
   return keyValues;
 }
 
-
-
-// these two (voteUp & voteDown) should be a single function w/ multiple args
-
-function voteUp() {
+function changeImp(card, vt) {
   var qualityArray = ['swill', 'plausible', 'genius'];
-  var currentQuality = $(this).siblings('.imp-val').text();
-  var currentIndex = qualityArray.indexOf(currentQuality);
-  console.log('up')
-
-  if(currentIndex < 2) {
-    currentIndex++;
-    currentQuality = $(this).siblings('.imp-val').text(qualityArray[currentIndex]);
-  }
-
-  var cardId = parseInt($(this).closest('article').attr('id'));
-  var cardObject = fromSto(cardId);
-  cardObject.quality = qualityArray[currentIndex];
-  toSto(cardObject);
-}
-
-function voteDown() {
-  var qualityArray = ['swill', 'plausible', 'genius'];
-  var currentQuality = $(this).siblings('.imp-val').text();
-  var currentIndex = qualityArray.indexOf(currentQuality);
-
-  console.log('down')
-
-  if(currentIndex > 0){
-    currentIndex--;
-    currentQuality = $(this).siblings('.imp-val').text(qualityArray[currentIndex]);
-  }
+  var $thisCrd = fromSto(getId(card))
+  var idx = qualityArray.indexOf($thisCrd.quality);
   
-  var cardId = parseInt($(this).closest('article').attr('id'));
-  var cardObject = fromSto(cardId);
-  cardObject.quality = qualityArray[currentIndex];
-  toSto(cardObject);
+  if(idx > 0 && vt === 'less'){
+    idx--;
+    $thisCrd.quality = qualityArray[idx];
+  } else if (idx < (qualityArray.length-1) && vt === 'more') {
+    idx++;
+    $thisCrd.quality = qualityArray[idx];
+  }
+
+  $(`#${$thisCrd.id} .imp-val`).text($thisCrd.quality)
+  toSto($thisCrd);
 }
 
 
@@ -118,11 +103,12 @@ function voteDown() {
 
 function searchString() {
   var cardObjectsArray = findExistingCards();
-  var userSearchInput = $('.srch-input').val().toLowerCase();
+  var srchInp = $('.srch-input').val().toLowerCase();
+
   var filteredCards = cardObjectsArray.filter(function (object){
     var lowercaseObjectBody = object['body'].toLowerCase();
     var lowercaseObjectTitle = object['title'].toLowerCase();
-    return lowercaseObjectBody.match(userSearchInput) || lowercaseObjectTitle.match(userSearchInput);
+    return lowercaseObjectBody.match(srchInp) || lowercaseObjectTitle.match(srchInp);
     }
   ) 
   clearAllCards();
