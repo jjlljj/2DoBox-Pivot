b6ca6cbe4dce90c9410b21183dc492c345c60304
James Logue <logueja@gmail.com>
2017-10-30 09:06:15 -0600

    Refactor edit card function

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ $(document).ready(populateExistingCards(findExistingCards()));
 var $crdCtnr = $('.crd-ctnr');
 
 $('.save-btn').on('click', newCrd);
+
 $crdCtnr.on('click', '.del-btn', delCrd);
 
 $crdCtnr.on('click', '.up-btn', function(){
@@ -12,11 +13,19 @@ $crdCtnr.on('click', '.down-btn', function() {
   changeImp(this, 'less')
 });
 
-$crdCtnr.on('blur', '.crd-title', changeIdeaTitle);
-$crdCtnr.on('blur', '.crd-task', changeIdeaBody);
-$crdCtnr.on('keypress', '.crd-title', updateTitle);
-$crdCtnr.on('keypress', '.crd-task', updateBody);
-$('srch-input').on('keyup', searchString);
+$crdCtnr.on('blur', '.crd-title', function() {
+  editCrd(this, 'title')
+});
+
+$crdCtnr.on('blur', '.crd-task', function() {
+  editCrd(this, 'body')
+});
+
+$crdCtnr.on('keypress', '.crd-title', unFocus);
+
+$crdCtnr.on('keypress', '.crd-task', unFocus);
+
+$('.srch-input').on('keyup', searchString);
 
 function ToDoCrd(id, title, body, quality) {
   this.id = id;
@@ -34,10 +43,6 @@ function newCrd() {
 }
 
 function toSto(crd) {
-      // IS IT BETTER TO DO THIS:
-//  var stringObj = JSON.stringify(crd);
-//  localStorage.setItem(crd.id, stringObj);
-     // OR THIS:
   localStorage.setItem(crd.id, JSON.stringify(crd));
 }
 
@@ -57,12 +62,43 @@ function delCrd() {
 }
 
 function clrInp() {
-  $('.title-input, .task-input').val('');
+  $('.title-input, .task-input, .srch-input').val('');
 }
 
+function changeImp(crd, vt) {
+  var qualityArray = ['swill', 'plausible', 'genius'];
+  var $thisCrd = fromSto(getId(crd));
+  var idx = qualityArray.indexOf($thisCrd.quality);
+  if (idx > 0 && vt === 'less') {
+    idx--;
+    $thisCrd.quality = qualityArray[idx];
+  } else if (idx < (qualityArray.length-1) && vt === 'more') {
+    idx++;
+    $thisCrd.quality = qualityArray[idx];
+  }
+  $(`#${$thisCrd.id} .imp-val`).text($thisCrd.quality)
+  toSto($thisCrd);
+}
 
+function unFocus(event) {
+  if (13 == event.keyCode) {
+    event.preventDefault();
+    $(this).blur()
+  }
+}
 
-//don't need dual loops of populateExisting && findExisting
+function editCrd(crd, edit) {
+  var text = $(crd).text();
+  var $thisCrd = fromSto(getId(crd));
+  if (edit === 'title'){
+    $thisCrd.title = text;
+  } else if (edit === 'body'){
+    $thisCrd.body = text;
+  }
+  toSto($thisCrd);
+}
+
+//don't need dual loops of populateExisting && findExisting.. also rename
 function populateExistingCards(keyValues) {
   for (var i = 0; i < keyValues.length; i++) {
     var thisCrd = fromSto(keyValues[i].id);
@@ -79,28 +115,7 @@ function findExistingCards() {
   return keyValues;
 }
 
-function changeImp(card, vt) {
-  var qualityArray = ['swill', 'plausible', 'genius'];
-  var $thisCrd = fromSto(getId(card))
-  var idx = qualityArray.indexOf($thisCrd.quality);
-  
-  if(idx > 0 && vt === 'less'){
-    idx--;
-    $thisCrd.quality = qualityArray[idx];
-  } else if (idx < (qualityArray.length-1) && vt === 'more') {
-    idx++;
-    $thisCrd.quality = qualityArray[idx];
-  }
-
-  $(`#${$thisCrd.id} .imp-val`).text($thisCrd.quality)
-  toSto($thisCrd);
-}
-
-
-
-
 // this can be shorterrr
-
 function searchString() {
   var cardObjectsArray = findExistingCards();
   var srchInp = $('.srch-input').val().toLowerCase();
@@ -115,55 +130,11 @@ function searchString() {
   populateExistingCards(filteredCards);
 }
 
+
+//is this even being called???
 function clearAllCards() {
   $('.crd-ctnr').text('');
 }
-
-
-
-/// refactor these two functions into one which takes title or body arg
-function changeIdeaTitle() {
-  var currentTitle = $(this).text();
-  var cardId = parseInt($(this).closest('article').attr('id'));
-  var cardObject = fromSto(cardId);
-  cardObject.title = currentTitle;
-  toSto(cardObject);
-}
-
-function changeIdeaBody() {
-  var currentBody = $(this).text();
-  var cardId = parseInt($(this).closest('article').attr('id'));
-  var cardObject = fromSto(cardId);
-  cardObject.body = currentBody;
-  toSto(cardObject);
-}
-
-
-/// also these... maybe these and prev 2 can be one function
-function updateTitle(event) {
-  if (13 == event.keyCode) {
-    event.preventDefault();
-    $('.crd-title').blur();
-    var cardId = parseInt($(this).closest('article').attr('id'));
-    var cardObject = fromSto(cardId);
-    cardObject.title = $(this).closest('.crd-title').text();
-    toSto(cardObject);
-  }
-}
-
-function updateBody(event) {
-  if (13 == event.keyCode) {
-    event.preventDefault();
-    $('.crd-task').blur();
-    var cardId = parseInt($(this).closest('article').attr('id'));
-    var cardObject = fromSto(cardId);
-    cardObject.body = $(this).closest('.crd-task').text();
-    toSto(cardObject);
-  }
-}
-
-
-
 
 function  prependCrd(crd) {
   $('.crd-ctnr').prepend(
